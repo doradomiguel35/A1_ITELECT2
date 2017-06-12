6e77b7b6ddc420d390b2d20fb2c10feb6651954f
doradomiguel35 <doradomiguel35@gmail.com>
2017-06-12 09:07:35 +0800

    commit

---
+++ server.js
@@ -0,0 +1,13 @@
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use(express.static(path.join(__dirname, 'public')));
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
